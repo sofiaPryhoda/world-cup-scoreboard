92cef455d21c4cdf4f26d78dccab62de00f4702a
sofiiapryhoda <sofia1806mollia@gmail.com>
2026-04-02 23:28:30 +0200

    add implementation for start match and validation for edge cases

--- src/main/java/com/sofiiapryhoda/scoreboard/ScoreBoard.java
+++ src/main/java/com/sofiiapryhoda/scoreboard/ScoreBoard.java
@@ -1,15 +1,32 @@
 package com.sofiiapryhoda.scoreboard;
 
+import com.sofiiapryhoda.scoreboard.exception.MatchAlreadyExistException;
 import com.sofiiapryhoda.scoreboard.model.Match;
+import com.sofiiapryhoda.scoreboard.validation.MatchInputValidator;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ScoreBoard {
     private final List<Match> matches = new ArrayList<>();
+    private final MatchInputValidator matchInputValidator = new MatchInputValidator();
 
     public void startMatch(String homeTeam, String awayTeam) {
+        this.matchInputValidator.validateMatch(homeTeam, awayTeam);
+        if (this.isTeamInActiveMatch(homeTeam, awayTeam)) {
+            throw new MatchAlreadyExistException("Cannot start match. " + homeTeam + " or " + awayTeam + " already playing");
+        }
+        this.matches.add(new Match(homeTeam, awayTeam, System.currentTimeMillis()));
+    }
 
+    private boolean isTeamInActiveMatch(String homeTeam, String awayTeam) {
+        return this.matches.stream()
+            .anyMatch(m ->
+                m.getHomeTeam().equalsIgnoreCase(homeTeam)
+                    || m.getHomeTeam().equalsIgnoreCase(awayTeam)
+                    || m.getAwayTeam().equalsIgnoreCase(awayTeam)
+                    || m.getAwayTeam().equalsIgnoreCase(homeTeam)
+            );
     }
 
     public List<Match> getMatches() {
---
+++ src/main/java/com/sofiiapryhoda/scoreboard/exception/InvalidMatchException.java
@@ -0,0 +1,7 @@
+package com.sofiiapryhoda.scoreboard.exception;
+
+public class InvalidMatchException extends RuntimeException {
+    public InvalidMatchException(String message) {
+        super(message);
+    }
+}
